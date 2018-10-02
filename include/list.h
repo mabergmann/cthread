9d1cfd71f62f6131a27e9ab0b644e65f7fccd1d5
Matheus Bergmann <mabergmann@inf.ufrgs.br>
2018-10-02 11:14:00 -0300

    Implemented cjoin

--- include/list.h
+++ include/list.h
@@ -6,8 +6,21 @@ struct list_node_s{
 };
 typedef struct list_node_s list_node;
 
+typedef struct join {
+  TCB_t* thread;
+  int tid;
+} join_t;
+
+struct list_node_join_s{
+  join_t* join;
+  struct list_node_join_s* next;
+};
+typedef struct list_node_join_s list_node_join;
+
 void append(list_node** list, void* new_value);
 
 TCB_t* pop_from_list(list_node** list, int tid);
 
 TCB_t* pop_from_queue(list_node** list);
+
+TCB_t* remove_join_from_list(list_node_join** list, int tid);
--- src/list.c
+++ src/list.c
@@ -46,3 +46,52 @@ TCB_t* pop_from_queue(list_node** list){
   *list = (*list)->next;
   return thread;
 }
+
+int is_tid_in_list(list_node* list, int tid){
+  while(list){
+    if(list->value->tid == tid)
+      return 1;
+    list = list->next;
+  }
+  return 0;
+}
+
+void append_join(list_node_join** list, join_t* new_value){
+  list_node_join* current;
+  list_node_join* new_node;
+  current = *list;
+  new_node = malloc(sizeof(list_node_join));
+  new_node->join = new_value;
+  new_node->next = NULL;
+  if(!current){
+    *list = new_node;
+  }
+  else {
+    while(current->next){
+      current = current->next;
+    }
+    current->next = new_node;
+  }
+}
+
+TCB_t* remove_join_from_list(list_node_join** list, int tid){
+  list_node_join* current;
+  list_node_join* previous=NULL;
+  TCB_t* thread;
+  current = *list;
+  while(current){
+    if(current->join->tid == tid){ // Found the node
+      if(!previous){
+	*list = current->next;
+	thread = current->join->thread;
+	return thread;
+      }else{
+	previous->next = current->next;
+	thread = current->join->thread;
+	return thread;
+      }
+    }
+    previous = current;
+    current = current->next;
+  }
+}
